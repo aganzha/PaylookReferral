cda63a059dbdfbbfd84b5d1ad46569109d2ba583
Ganzha Aleksey <aganzha@yandex.ru>
2014-03-15 14:30:47 +0300

    init

---
+++ PaylookReferral/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaylookReferral.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            using (var context = new Entities.Entities())
+            {
+                var cli = context.Clients.Where(cl => cl.Id == 1).FirstOrDefault();
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
